01366903abad6e111b12282d0cd2d69bbbd47b73
GiordanoT <giordanotinella@gmail.com>
2023-09-14 11:30:02 +0200

    proxy with env variable

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,18 +1,11 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        '/classes',
+        '/memorec/*',
         createProxyMiddleware({
-            target: 'http://localhost:8080',
-            changeOrigin: true,
-        })
-    );
-    app.use(
-        '/structuralFeatures',
-        createProxyMiddleware({
-            target: 'http://localhost:8080',
-            changeOrigin: true,
+            target: process.env['REACT_APP_MEMOREC'],
+            changeOrigin: true
         })
     );
 };
