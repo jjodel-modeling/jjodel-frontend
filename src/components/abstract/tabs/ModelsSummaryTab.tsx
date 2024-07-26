2ed92d92c793597c0ebc0ec319971b6aec106476
Alfonso Pierantonio <alfonso.pierantonio@univaq.it>
2024-07-26 16:05:48 +0200

    restyling summary progetto

--- src/components/abstract/tabs/ModelsSummaryTab.tsx
+++ src/components/abstract/tabs/ModelsSummaryTab.tsx
@@ -4,15 +4,18 @@ import React, {Dispatch, ReactElement, useState, ReactNode} from 'react';
 import {connect} from 'react-redux';
 import type {Dictionary, FakeStateProps} from '../../../joiner/types';
 import DockManager from "../DockManager";
-
-/* test */
-
+import jj from '../../../static/img/jj.png';
 
 type Props = {
     key: string;
     name: string;
     data: any;
+    metamodels: any;
+    models: any;
 };
+
+
+
 
 const Project = (props: Props) => {
 
@@ -25,16 +28,47 @@ const Project = (props: Props) => {
     return (
         <React.Fragment>
             {!edit ? 
-                <h1 onClick={editToggle} className={'p-3'}>{props.name}</h1> :
-                <h1 onBlur={editToggle} className={'p-3'}><Input key={props.key} field={'name'} data={props.data}/></h1>
+                <h2 onClick={editToggle} className={'p-3'}>{props.name}<img style={{float: 'right'}}height={50} src={jj} /></h2> :
+                <h2 onBlur={editToggle} className={'p-3'}><Input key={props.key} field={'name'} data={props.data}/></h2>
             }
-            
+            <p className='p-3'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            </p>
+            <div className='row p-3'>
+                <div className={'col-sm'}>
+                    <p>
+                    {props.data.type == "public" && <React.Fragment><i className="bi bi-unlock"></i> public project</React.Fragment>}
+                    {props.data.type == "private" && <React.Fragment><i className="bi bi-lock"></i> private project</React.Fragment>}
+                    {props.data.type == "collaborative" && <React.Fragment><i className="bi bi-diagram-3"></i> collaborative project</React.Fragment>}
+                    </p>
+                </div>
+                <div className={'col-sm'}>
+                    <p>
+                    {(props.data.type === 'collaborative') ?
+                        <div><i className="bi bi-diagram-3"></i> online users: <strong>{props.data.onlineUsers}</strong></div> :
+                        <div><i className="bi bi-diagram-3"></i> online users: <strong>only you.</strong></div>
+                    }
+                    </p>
+                </div>
+                <div className='row p-3'>
+                    
+                        <h4>Metamodels ({props.metamodels.length})</h4>
+                        <section>
+                            {props.metamodels.map(m2Row)}
+                        </section>
+                </div>
+                <div className={'row p-3'}>
+                        <h4>Models ({props.models.length})</h4>
+                        <section>
+                            {props.models.map(m1Row)}
+                        </section>
+
+                </div>
+            </div>
         </React.Fragment>
     );
 }
-
-
-
 
 function m2Row(model: LModel) { return mRow(model, false) }
 function m1Row(model: LModel) { return mRow(model, true) }
@@ -43,13 +77,19 @@ function m1Row(model: LModel) { return mRow(model, true) }
 function mRow(model: LModel, showInstanceOf: boolean = false) {
     if(!model) return(<></>);
     return (
-        <p className={'ms-3 d-block'} key={model.id} onClick={()=>DockManager.open2(model)} style={{cursor: 'pointer'}}>
+        <p className={'d-block'} key={model.id} onClick={()=>DockManager.open2(model)} style={{cursor: 'pointer'}}>
             <i className="bi bi-folder"></i> {model.name}{
                 showInstanceOf && <><span className={'text-success'}> {model.instanceof ? 'conforming to' : 'is shapeless'}</span> {model.instanceof?.name}</>
             }
-           {/* <button style={{borderRadius: '999px'}} className={'ms-2 btn btn-primary p-0'} onClick={() => DockManager.open2(model)}>
-                open
-            </button>*/}
+            <i className="bi bi-chevron-down hoverable">
+                <div className="content context-menu">
+                    <div className={'col item'}>Open</div>
+                    <div className={'col item'}>Duplicate</div>
+                    <div className={'col item'}>Close</div>
+                    <hr className={'my-1'} />
+                    <div className={'col item'}>Delete</div>
+                </div>
+            </i>
         </p>)
 }
 
@@ -68,30 +108,8 @@ function InfoTabComponent(props: AllProps) {
 
     return(<div className={'p-3 summary w-75'}>
         <div>
-            <Project key={project.id} name={project.name} data={project}/>
-            <p className='p-3'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            </p>
-            <p className={'p-3'}>
-                {project.type == "public" && <React.Fragment><i className="bi bi-unlock"></i> public project</React.Fragment>}
-                {project.type == "private" && <React.Fragment><i className="bi bi-lock"></i> private project</React.Fragment>}
-                {project.type == "collaborative" && <React.Fragment><i className="bi bi-diagram-3"></i> collaborative project</React.Fragment>}
-            </p>
+            <Project key={project.id} name={project.name} data={project} metamodels={metamodels} models={models}/>
         </div>
-
-        {(project.type === 'collaborative') &&
-            <b className={'d-block'}><label className={'text-primary '}>Online Users:</label> {project.onlineUsers}</b>
-        }
-
-        <h4 className={'p-3'}>Metamodels ({metamodels.length})</h4>
-        <section>
-            {metamodels.map(m2Row)}
-        </section>
-        <h4 className={'p-3'}>Models ({models.length})</h4>
-        <section>
-            {models.map(m1Row)}
-        </section>
     </div>);
 }
 
