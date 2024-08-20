30b66ac07d3e0bbc339c6ae61510fb5e40444e9d
Alfonso Pierantonio <alfonso.pierantonio@univaq.it>
2024-08-20 11:00:10 +0200

    fix viewpoint/events

--- src/components/widgets/Widgets.tsx
+++ src/components/widgets/Widgets.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import './widgets.scss';
 import { event } from 'jquery';
+import { int } from '../../joiner/types';
+import { windoww } from '../../joiner/types';
+import { SetRootFieldAction } from '../../joiner';
+import { MapStateToProps } from 'react-redux';
+import { InputMapStateToProps } from '../forEndUser/Input';
 
 type ToggleValues = {
     true: string;
@@ -38,3 +43,30 @@ export const Toggle = (props: ToggleProps) => {
     );
 }
 
+type RangeProps = {
+    name: string;
+    min: int;
+    max: int;
+    step?: int;
+    label?: string;
+
+}
+
+export const Range = (props: RangeProps) => {
+
+    return (<div className={'range'}>
+        /* <div className={'level'}>{level}</div> */
+        <input 
+            name={props.name} 
+            id={props.name} 
+            min={props.min} 
+            max={props.max} 
+            type="range" 
+            step={props.step} 
+            /* value={level} */
+            onChange={(e)=>{SetRootFieldAction.new(props.name, e.target.value)}} />
+        
+        <div className={'tip'}>Abstraction1</div>
+    </div>);
+
+}
