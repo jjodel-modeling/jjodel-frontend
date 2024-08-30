85dcf287888172fee0f2f3f2862782f2848eb4f2
Alfonso Pierantonio <alfonso.pierantonio@univaq.it>
2024-08-30 11:07:02 +0200

    sub-tab template fixes

--- src/pages/components/cards/Cards.tsx
+++ src/pages/components/cards/Cards.tsx
@@ -18,7 +18,7 @@ export const Cards = (props: CardsType): any => {
 
 type CardType = {
     icon: "add" | "import" | "question" | "gettingstarted" | "alexa";
-    style?: "blue" | "red" | "dark" | "clear" | "rainbow" | "red-orange" | "default";
+    style?: "ottanio" | "red" | "dark" | "clear" | "rainbow" | "red-orange" | "default";
     title: string;
     subtitle: string;
     action?: MouseEventHandler;
