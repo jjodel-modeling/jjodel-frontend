8167a9debcbee023f045990fe68ccaed094b4417
Alfonso Pierantonio <alfonso.pierantonio@univaq.it>
2024-09-11 09:55:06 +0200

    login refinement

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -6,6 +6,7 @@ import {useNavigate} from "react-router-dom";
 import {AuthApi} from "../api/persistance";
 
 import logo from '../static/img/jjodel.jpg';
+import { Tooltip } from '../components/forEndUser/Tooltip';
 
 function AuthPage(): JSX.Element {
     const [isRegister, setIsRegister] = useStateIfMounted(false);
@@ -57,21 +58,126 @@ function AuthPage(): JSX.Element {
         U.refresh();
     }
 
-    return(<section className={'w-100 h-100 login bg-3'}>
+    return(<section className={`w-100 h-100 login bg-3 ${isRegister && 'register'}`}>
         <form className={'d-block bg-white rounded border mx-auto w-fit px-5 py-4 mt-5'} onSubmit={onSubmit}>
             <label className={'fs-1 d-block text-center text-primary login-header'}>
                 {isRegister ? 'Create an Account' : 'Sign In'}
             </label>
-            <hr />
-            <input className={'w-100 input w-fit d-block mx-auto mt-3'} placeholder={'Email'}
-                   value={email} onChange={e => setEmail(e.target.value)} type={'email'}  required={true} />
-            {isRegister &&
-                <input className={'w-100 input w-fit d-block mx-auto mt-2'} placeholder={'Username'}
-                       value={username} onChange={e => setUsername(e.target.value)} type={'text'} required={true} />
-            }
-            <input className={'w-100 input w-fit d-block mx-auto  mt-2'} placeholder={'Password'}
-                   value={password} onChange={e => setPassword(e.target.value)} type={'password'} required={true} />
-            <button className={'d-block btn btn-primary p-1 mx-auto mt-3 login-button'} type={'submit'}>Login</button>
+
+            {isRegister ? <>
+                <Tooltip tooltip={<div style={{padding: '10px', maxWidth: '600px'}}><h6>First Name</h6>Your first name will be visible to others whenever you interact with them, such as during collaboration on shared projects.</div>} >
+                    <label>
+                        First Name
+                        <input className={'w-100 input w-fit d-block mx-auto mt-2'} 
+                            value={username} 
+                            onChange={e => setUsername(e.target.value)} 
+                            type={'text'} required={true} 
+                        />
+                    </label>
+                </Tooltip>
+
+                <label>
+                    Last Name (optional) 
+                    <input className={'w-100 input w-fit d-block mx-auto mt-2'} 
+                        value={username} 
+                        onChange={e => setUsername(e.target.value)} 
+                        type={'text'} required={true} 
+                    />
+                </label>
+                <Tooltip tooltip={<div style={{padding: '10px', maxWidth: '600px'}}><h6>Affiliation</h6>Your affiliation refers to the organization, institution, or company you’re associated with, will be displayed in relevant contexts like project collaborations or professional interactions, and will help us keep track of where jjodel is being used.</div>} ><label>
+                    Affiliation 
+                    <input className={'w-100 input w-fit d-block mx-auto mt-2'} 
+                        value={username} 
+                        onChange={e => setUsername(e.target.value)} 
+                        type={'text'} required={true} 
+                    />
+                    </label>
+                </Tooltip>
+                <label>
+                    Country (optional)
+                    <input className={'w-100 input w-fit d-block mx-auto mt-2'} 
+                        value={username} 
+                        onChange={e => setUsername(e.target.value)} 
+                        type={'text'} required={true} 
+                    />
+                </label>
+                <Tooltip tooltip={<div style={{padding: '10px', maxWidth: '600px'}}><h6>Email</h6>Your email address will be used for communication, notifications, and to identify you in the system, but it won’t be shared publicly without your consent.</div>} >
+                    <label>
+                        Email 
+                        <input className={'w-100 input w-fit d-block mx-auto mt-2'} 
+                            value={username} 
+                            onChange={e => setUsername(e.target.value)} 
+                            type={'text'} required={true} 
+                        />
+                    </label>
+                </Tooltip>
+                <br /><br /><br />
+                
+                <label>
+                    Password 
+                    <input className={'w-100 input w-fit d-block mx-auto mt-2'} 
+                        value={password} 
+                        onChange={e => setUsername(e.target.value)} 
+                        type={'password'} required={true} 
+                    />
+                </label>
+
+            
+                <label>
+                    Confirm Password 
+                    <input className={'w-100 input w-fit d-block mx-auto mt-2'} 
+                        value={password} 
+                        onChange={e => setUsername(e.target.value)} 
+                        type={'password'} required={true} 
+                    />
+                </label>
+
+                <br /><br /><br />
+                <Tooltip tooltip={<div style={{padding: '10px', maxWidth: '600px'}}><h6>Email</h6>Your email address will be used for communication, notifications, and to identify you in the system, but it won’t be shared publicly without your consent.</div>} >
+                    <label>
+                        
+                        <input className={'checkbox'} 
+                            placeholder={'newsletter'}
+                            value={username} 
+                            onChange={e => setUsername(e.target.value)} 
+                            type={'checkbox'}
+                            style={{outline: 'none', marginTop: '10px', float: 'left'}}
+                        />
+                        <div style={{display: 'block', width: '90%', float: 'left', marginBottom: '10px',     paddingLeft: '10px'}}>Newsletter. Subscribe to the newsletter to receive updates and news. You can manage your registration preferences at any time. </div>
+                    </label>
+                    
+                </Tooltip>
+                <br />
+                <div style={{width: '100%', textAlign: 'center'}}>
+                    By proceeding you accept the terms and conditions.
+                </div>
+                <button className={'d-block btn btn-primary p-1 mx-auto mt-3 login-button'} type={'submit'}>
+                    Create
+                </button>
+            </>
+            : 
+            <>
+                <input className={'w-100 input w-fit d-block mx-auto mt-3'} 
+                    value={email} 
+                    onChange={e => setEmail(e.target.value)} type={'email'}  
+                    required={true} 
+                />
+                <input className={'w-100 input w-fit d-block mx-auto  mt-2'} 
+                    value={password} 
+                    onChange={e => setPassword(e.target.value)} 
+                    type={'password'} 
+                    required={true} 
+                />
+                <button className={'d-block btn btn-primary p-1 mx-auto mt-3 login-button'} type={'submit'}>
+                    Login
+                </button>
+            </>}
+
+
+
+            
+            
+            
             <label className={'mt-3 d-block text-center'}>
                 {isRegister ? 'Already have an account?' : 'Don\'t have an account?'}
                 <b tabIndex={-1} onClick={e => setIsRegister(!isRegister)} className={'ms-1 text-primary text-decoration-none cursor-pointer login-link'}>
