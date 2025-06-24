8705534c1cf43660a7e4ef20e0f12389bc87e986
Damiano Di Vincenzo <damianodivincenzo7@gmail.com>
2025-06-24 03:07:43 +0200

    Update DTO.ts

--- frontend/src/api/DTO/DTO.ts
+++ frontend/src/api/DTO/DTO.ts
@@ -9,10 +9,10 @@ export abstract class DTO<S extends GObject = GObject>{
             this._dto_copyInterections(src, setFields)
             this._dto_convert(src, setFields);
             let diff = U.arrayDifference(Object.keys(this), Object.keys(setFields));
-            Log.eDevv('incomplete conversion to DTO', {dto:this, src, diff});
+            Log.eDevv('incomplete conversion to DTO', {dto: {...this}, setFields, diff, src});
         }
         catch(e){
-            Log.eDevv('failed to convert to DTO',{dto:this, src, e});
+            Log.eDevv('failed to convert to DTO',{dto: {...this}, src, e});
         }
     }
     protected _dto_set<K extends keyof this = string & keyof this>(k: K,val: this[K], setFields: Dictionary<string, boolean>){
