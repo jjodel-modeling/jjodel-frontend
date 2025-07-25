329f9a7a5331850ff2abc6b548781f41428d0e82
Alfonso Pierantonio <alfonso.pierantonio@univaq.it>
2025-07-25 17:55:16 +0200

    iconcine su tab dell'editor mm e m

--- frontend/src/components/abstract/tabs/TabDataMaker.tsx
+++ frontend/src/components/abstract/tabs/TabDataMaker.tsx
@@ -2,12 +2,13 @@ import {DModel, LModel} from '../../../model/logicWrapper';
 import {TabData} from 'rc-dock';
 import MetamodelTab from './MetamodelTab';
 import ModelTab from './ModelTab';
+import { TbSquareRoundedLetterM, TbSquareRoundedLetterMFilled } from 'react-icons/tb';
 
 class TabDataMaker {
     static metamodel (model: DModel|LModel): TabData {
         return {
             id: model.id,
-            title: <div className={"active-on-mouseenter"}>{model.name}</div>,
+            title: <div className={"active-on-mouseenter"}><TbSquareRoundedLetterMFilled style={{fontSize: '1.2em'}}/> {model.name}</div>,
             group: 'models',
             closable: true,
             content: <MetamodelTab modelid={model.id} key={model.id} />
@@ -16,7 +17,7 @@ class TabDataMaker {
     static model(model: DModel|LModel): TabData {
         return {
             id: model.id,
-            title: <div className={"active-on-mouseenter"}>{model.name}</div>,
+            title: <div className={"active-on-mouseenter"}><TbSquareRoundedLetterM style={{fontSize: '1.2em'}}/> {model.name}</div>,
             group: 'models',
             closable: true,
             content: <ModelTab modelid={model.id} metamodelid={(model.instanceof as any)?.id || model.instanceof} />
