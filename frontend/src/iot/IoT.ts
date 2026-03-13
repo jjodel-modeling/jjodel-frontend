9162a5b6a6a50ec74b8789ab8d898547dbb817f5
Damiano Di Vincenzo <naraku7686@live.it>
2026-03-13 15:55:04 +0100

    Update IoT.ts

--- frontend/src/iot/IoT.ts
+++ frontend/src/iot/IoT.ts
@@ -1,7 +1,7 @@
 import {io} from 'socket.io-client';
-import {U} from "../joiner";
 class IoT {
-    static client = io(`${U.env('JODEL_IOT')}`, {path: '/iot', autoConnect: false});
+    //@ts-ignore
+    static client = io(`${window.U.env('JODEL_IOT')}`, {path: '/iot', autoConnect: false});
 }
 
 export default IoT;
