235eadfbeead24d69c7cd1a02fbdbb878e205190
Damiano Di Vincenzo <naraku7686@live.it>
2026-05-21 14:52:00 +0200

    Fix bug #71

--- frontend/src/components/editors/views/data/PaletteData.tsx
+++ frontend/src/components/editors/views/data/PaletteData.tsx
@@ -1,5 +1,4 @@
 /* Viewpoints > Style */
-
 import React, {Dispatch, ReactElement, ReactNode, Ref, RefObject, SyntheticEvent, useState, useRef, useEffect} from 'react';
 import {connect} from "react-redux";
 import {useStateIfMounted} from "use-state-if-mounted";
